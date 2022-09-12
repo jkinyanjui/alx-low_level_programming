f50f982e7ca9d92dbaf868c1a4d2ae4c941befe0
Wanjiru <joywkinyanjui@gmail.com>
2022-09-12 07:28:58 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+
+/*
+ * Basic print function
+ */
+int main(void)
+{
+	printf("Size of a char: %i bytes(s)\n", sizeof(char));
+	printf("Size of an int: %i bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %i bytes(n)\n", sizeof(long));
+	printf("Size of a float: %i bytes(n)\n", sizeof(float));
+
+	return (0);
+}
