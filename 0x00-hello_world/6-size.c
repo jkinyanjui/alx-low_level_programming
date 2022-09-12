eb3d7ffec5eebec20551d1dccb71cd3a89e3e448
Wanjiru <joywkinyanjui@gmail.com>
2022-09-12 07:32:41 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,17 @@
 
-/*
- * Basic print function
+/**
+ *
+ * main - Entry point
+ *
+ *  
+ *
+ *    Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("Size of a char: %i bytes(s)\n", sizeof(char));
 	printf("Size of an int: %i bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %i bytes(n)\n", sizeof(long));
+	printf("Size of a long long int: %i bytes(n)\n", sizeof(long));
 	printf("Size of a float: %i bytes(n)\n", sizeof(float));
 
 	return (0);
