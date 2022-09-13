f02078b5e00cec02e145661600d8eb7cd6d8017d
Wanjiru <joywkinyanjui@gmail.com>
2022-09-13 16:22:14 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *
+ * main - Entrypoint
+ *
+ *  
+ *
+ * Return: Always 0 (success)
+ *
+ * 
+ *
+ * */
+
+int main(void)
+
+{
+	int letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
