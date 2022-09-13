88602c2980497df80872fb7858a6d7f141fdbf1e
Wanjiru <joywkinyanjui@gmail.com>
2022-09-13 16:32:38 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
-
 /**
- *
  * main - Entrypoint
- *
- *  
  *
  * Return: Always 0 (success)
  *
- * 
- *
- * */
+ */
 
 int main(void)
-
 {
 	int letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		letter = tolower(letter);
